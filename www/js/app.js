2ebdb11ee9091399f588021528626869f6f3758c
patrickeddy <p@patrickeddy.com>
2014-10-24 11:37:24 -0700

    Fixed cordova keyboard plugin crashes.

--- www/js/app.js
+++ www/js/app.js
@@ -7,83 +7,83 @@
 // 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
-.run(function ($ionicPlatform) {
-    $ionicPlatform.ready(function () {
-        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-        // for form inputs)
-        if (window.cordova && window.cordova.plugins.Keyboard) {
-            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-        }
-        if (window.StatusBar) {
-            // org.apache.cordova.statusbar required
-            StatusBar.styleDefault();
-        }
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
 
-        // Initiate FastClick
-        window.addEventListener('load', function(){
-          FastClick.attach(document.body);
-        }, false);
-    });
+    // Initiate FastClick
+    window.addEventListener('load', function() {
+      FastClick.attach(document.body);
+    }, false);
+  });
 })
 
-.config(function ($stateProvider, $urlRouterProvider) {
+.config(function($stateProvider, $urlRouterProvider) {
 
-    // Ionic uses AngularUI Router which uses the concept of states
-    // Learn more here: https://github.com/angular-ui/ui-router
-    // Set up the various states which the app can be in.
-    // Each state's controller can be found in controllers.js
-    $stateProvider
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
 
-    // setup an abstract state for the tabs directive
+  // setup an abstract state for the tabs directive
     .state('tab', {
-        url: "/tab",
-        abstract: true,
-        templateUrl: "templates/tabs.html"
-    })
+    url: "/tab",
+    abstract: true,
+    templateUrl: "templates/tabs.html"
+  })
 
-    // Each tab has its own nav history stack:
+  // Each tab has its own nav history stack:
 
-    .state('tab.dash', {
-        url: '/dash',
-        views: {
-            'tab-dash': {
-                templateUrl: 'templates/tab-dash.html',
-                controller: 'DashCtrl'
-            }
-        }
-    })
+  .state('tab.dash', {
+    url: '/dash',
+    views: {
+      'tab-dash': {
+        templateUrl: 'templates/tab-dash.html',
+        controller: 'DashCtrl'
+      }
+    }
+  })
 
-    .state('tab.players', {
-        url: '/players',
-        views: {
-            'tab-players': {
-                templateUrl: 'templates/tab-players.html',
-                controller: 'PlayersCtrl'
-            }
-        }
-    })
+  .state('tab.players', {
+    url: '/players',
+    views: {
+      'tab-players': {
+        templateUrl: 'templates/tab-players.html',
+        controller: 'PlayersCtrl'
+      }
+    }
+  })
 
-    .state('tab.new-player', {
-        url: '/players/new-player',
-        views: {
-            'tab-players': {
-                templateUrl: 'templates/tab-new-player.html',
-                controller: 'PlayersCtrl'
-            }
-        }
-    })
+  .state('tab.new-player', {
+    url: '/players/new-player',
+    views: {
+      'tab-players': {
+        templateUrl: 'templates/tab-new-player.html',
+        controller: 'PlayersCtrl'
+      }
+    }
+  })
 
-    .state('tab.results', {
-        url: '/results',
-        views: {
-            'tab-results': {
-                templateUrl: 'templates/tab-results.html',
-                controller: 'ResultsCtrl'
-            }
-        }
-    });
+  .state('tab.results', {
+    url: '/results',
+    views: {
+      'tab-results': {
+        templateUrl: 'templates/tab-results.html',
+        controller: 'ResultsCtrl'
+      }
+    }
+  });
 
-    // if none of the above states are matched, use this as the fallback
-    $urlRouterProvider.otherwise('/tab/dash');
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/tab/dash');
 
 });
